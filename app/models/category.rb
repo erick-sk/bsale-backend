530b318c5fc6e8426610171ff0a4f995242383df
Erick Saavedra <esvdrah@gmail.com>
2022-08-31 15:00:12 -0500

    Add associations

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,3 +1,6 @@
 class Category < ApplicationRecord
   self.table_name = 'category'
+
+  # Associations
+  has_many :products, foreign_key: :category
 end
--- app/models/product.rb
+++ app/models/product.rb
@@ -1,3 +1,6 @@
 class Product < ApplicationRecord
   self.table_name = 'product'
+
+  # Associations
+  belongs_to :category, foreign_key: :category
 end
