0848fd04d6b2f3f2f2aaccae1d5f169c851efebb
Erick Saavedra <esvdrah@gmail.com>
2022-09-01 19:09:13 -0500

    Find products by category id

--- app/querys/find_products.rb
+++ app/querys/find_products.rb
@@ -8,6 +8,7 @@ class FindProducts
   def call(params = {})
     scoped = products
     scoped = filter_by_query_text(scoped, params[:query_text])
+    scoped = filter_by_category_id(scoped, params[:category_id])
   end
 
   private
@@ -20,5 +21,11 @@ class FindProducts
       return scoped unless query_text.present?
 
       scoped.where('name LIKE?', "%#{query_text}%")
+    end
+
+    def filter_by_category_id(scoped, category_id)
+      return scoped unless category_id.present?
+
+      scoped.where(category: category_id)
     end
 end
